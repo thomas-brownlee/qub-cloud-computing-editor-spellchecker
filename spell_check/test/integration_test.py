3f66b6a1c4e42ef4dfa2772a99263b2d0839ef40
40324582 <tbrownlee02@qub.ac.uk>
2024-12-05 22:35:07 +0000

    adding integration tests checking the behaviour if there is one spelling mistake

--- spell_check/test/integration_test.py
+++ spell_check/test/integration_test.py
@@ -97,3 +97,26 @@ def test_spell_check_endpoint_string_pram_is_valid_string_with_no_spelling_mista
         'answer': 0,
     }
     assert response.json == expected_data
+
+def test_spell_check_endpoint_string_pram_is_valid_string_with_spelling_mistakes(client):
+    """
+    Test the /api/spell-check endpoint.
+
+    When parameter is empty
+    """
+    response = client.get('/api/spell-check?text=helo world')
+
+    # Assert the status code is 200
+    assert response.status_code == 200
+
+    # Assert the response headers return json and CORS is allowed
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.headers["Access-Control-Allow-Origin"] == "*"
+
+    # Assert the response JSON
+    expected_data = {
+        'error': False,
+        'string': 'spelling mistake helo on line 0 did you mean help',
+        'answer': 1,
+    }
+    assert response.json == expected_data
