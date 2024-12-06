fc466423d192ec6d8ace8a6837fc82115356d3bf
40324582 <tbrownlee02@qub.ac.uk>
2024-12-06 02:55:34 +0000

    testing when string does have  a spelling mistake

--- spell_check/test/end_to_end_test.py
+++ spell_check/test/end_to_end_test.py
@@ -52,6 +52,20 @@ class TestSpellChecker(unittest.TestCase):
         )
         self.assertEqual(json_data["answer"], 0)
 
+    def test_spell_check_endpoint_normal_with_spelling_mistake(self):
+        """Runs a test for the spell-check endpoint with a spelling mistake"""
+        url = f"{test_url}/api/spell-check?text=hello helo"
+        response = requests.get(url, timeout=10)
+
+        self.assertEqual(response.status_code, 200)
+
+        json_data = response.json()
+
+        expected_string = "spelling mistake helo on line 0 did you mean help"
+        self.assertEqual(False, json_data["error"])
+        self.assertEqual(expected_string, json_data["string"])
+        self.assertEqual(json_data["answer"], 1)
+
 
 if __name__ == "__main__":
     unittest.main()
